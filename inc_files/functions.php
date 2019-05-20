882dcd8e331a80058cb5a6ffdca4f2fa4fb0a3f3
stefan-nicolau <50801218+stefan-nicolau@users.noreply.github.com>
2019-05-20 09:21:07 +0300

    Add files via upload

---
+++ inc_files/functions.php
@@ -0,0 +1,32 @@
+<?
+
+function safe_bvars($var){ 
+	
+	$con = mysqli_connect("localhost",'scoaladk_site', 'D6[_#l{kBf.g',"scoaladk_site");
+	$ivar = trim(mysqli_real_escape_string($con,strip_tags($var)));
+
+	//$ivar = str_replace(" ", "", $ivar); 
+
+	return $ivar;
+
+}
+
+function sent_bmail($email, $subject, $mesaj_email){
+
+
+
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $headers .= "From: Scoala de Make-up <noreply@scoalademakeup.ro>\n";
+
+    mail($email, $subject, $mesaj_email, $headers);
+	
+
+}
+
+
+
+?>
